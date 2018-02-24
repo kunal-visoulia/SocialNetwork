517f8d10e0dfb0a410152931bdc9f26330cee0d7
Kunal Visoulia <kunalvisoulia@gmail.com>
2018-02-24 22:18:14 +0530

    move a network from an unstable state to a stable state

--- src/BalanceTheorem(+ve and -ve relationships)/stable_signed.py
+++ src/BalanceTheorem(+ve and -ve relationships)/stable_signed.py
@@ -56,5 +56,80 @@ def count_unstable(all_signs):
             unstable+=1
     print ('Number of stable triangles out of ',stable+unstable,' are ',stable)
     print ('Number of unstable triangles out of ',stable+unstable,' are ',unstable)
-
+    return unstable
 unstable=count_unstable(all_signs)
+unstable_track=[unstable]
+#5. do it till there are unstable triangles in the graph(5.1 to 5.3)
+
+#5.1 choose an unstable triangle
+def move_a_tri_to_stable(G,triangles_list,all_signs):
+    found_unstable=False
+    while(found_unstable==False):
+        index=rn.randint(0,len(triangles_list)-1)
+        #check for triangle at position=index , unstable or stable
+        if all_signs[index].count('+')==2 or all_signs[index].count('+')==0:
+            found_unstable=True
+        else:
+            continue
+    #5.2 make the triangle stable
+    #there are 3 possible corresponding stable states for an unstable state(there are 2 possible unstable states)
+    #for #+=2, inversion of sign  of any one side leads to a stable state
+    #for #+=0, -ve sign of any one side to =ve leads to a stable sate
+    r=rn.randint(1,3)#for chossing one out of 3 possible states
+    
+    if all_signs[index].count('+')==2:
+        if r==1:
+            if G[triangles_list[index][0]][triangles_list[index][1]]['sign']=='+':
+                G[triangles_list[index][0]][triangles_list[index][1]]['sign']='-'
+            elif G[triangles_list[index][0]][triangles_list[index][1]]['sign']=='-':
+                G[triangles_list[index][0]][triangles_list[index][1]]['sign']='+'
+                
+        elif r==2:
+            if G[triangles_list[index][1]][triangles_list[index][2]]['sign']=='+':
+                G[triangles_list[index][1]][triangles_list[index][2]]['sign']='-'
+            elif G[triangles_list[index][1]][triangles_list[index][2]]['sign']=='-':
+                G[triangles_list[index][1]][triangles_list[index][2]]['sign']='+'
+         
+        elif r==3:
+            if G[triangles_list[index][0]][triangles_list[index][2]]['sign']=='+':
+                G[triangles_list[index][0]][triangles_list[index][2]]['sign']='-'
+            elif G[triangles_list[index][0]][triangles_list[index][2]]['sign']=='-':
+                G[triangles_list[index][0]][triangles_list[index][2]]['sign']='+'           
+    
+    if all_signs[index].count('+')==0:
+        if r==1:
+            G[triangles_list[index][0]][triangles_list[index][1]]['sign']='+'
+                
+        elif r==2:
+            G[triangles_list[index][1]][triangles_list[index][2]]['sign']='+'
+         
+        elif r==3:
+            G[triangles_list[index][0]][triangles_list[index][2]]['sign']='+'  
+                  
+    return G
+    
+while(unstable!=0):
+    G=move_a_tri_to_stable(G,triangles_list,all_signs)
+    #after a triangle is made stable and graph is updated, adjacent triangle may get unstable 
+    # and thus signs of the triangles need to be reevaluated
+    #we can choose to reevaluate signs of all triangles(chosen way) or only adjacent triangles
+    all_signs=get_signs_of_triangles(triangles_list,G)
+    #5.3 update unstable
+    unstable=count_unstable(all_signs)
+    unstable_track.append(unstable)
+
+#how the number of unstable triangles change when we move one unstable triangle to
+#a stable state
+raw_input()    
+plt.bar([i for i in range(len(unstable_track))],unstable_track)
+plt.xlabel('no of iterations of while loop')
+plt.ylabel('no. of unstable triangles')
+plt.show()
+    
+    
+    
+
+
+
+
+
