3db0d4750f3a2b7278695c6255600109ceafcba6
Kunal Visoulia <kunalvisoulia@gmail.com>
2018-04-09 21:21:13 +0530

    add weak ties between random nodes

--- src/Week 12- Small world Networks/ring.py
+++ src/Week 12- Small world Networks/ring.py
@@ -1,6 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import random
 def add_edges(G):
     list_nodes=G.nodes()
     n=G.number_of_nodes()
@@ -24,15 +24,28 @@ def add_edges(G):
             G.add_edge(list_nodes[i],target)
         else:
             G.add_edge(list_nodes[i],target)
+
+def add_long_link(G):
+    v1=random.choice(G.nodes())
+    v2=random.choice(G.nodes())
+    while(v1==v2):
+        v1=random.choice(G.nodes())
+        v2=random.choice(G.nodes())
+    G.add_edge(v1,v2)
             
 G=nx.Graph()
-G.add_nodes_from(range(0,50))
+G.add_nodes_from(range(0,100))
 
 add_edges(G)  #add ties based on homophily
+
+#prints ties
 for each in G.nodes():
     print each,':',
     for each1 in G.neighbors(each):
         print each1,
     print '\n'
+    
+add_long_link(G)
+
 nx.draw(G,with_labels=1)
 plt.show()
