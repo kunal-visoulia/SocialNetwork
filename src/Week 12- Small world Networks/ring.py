8d5bc5dd731f6e871499e4a5c954c8ea366fe25f
Kunal Visoulia <kunalvisoulia@gmail.com>
2018-04-09 21:16:15 +0530

    Make homophily based ties

--- src/Week 12- Small world Networks/ring.py
+++ src/Week 12- Small world Networks/ring.py
@@ -1,6 +1,38 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+
+def add_edges(G):
+    list_nodes=G.nodes()
+    n=G.number_of_nodes()
+    #every nodes in this list is supposed to be connected to 2 nodes on right and left each
+    for i in range(0,len(list_nodes)):
+        #print list_nodes[i], list_nodes[i+1], list_nodes[i-1], list_nodes[i+2], list_nodes[i-2]
+        #-ve indices pose no problem, but
+        #when i+1 or i+2->n, we need to set this value to 0
+        #                ->n+1, we need to set this to 1 
+        G.add_edge(list_nodes[i],list_nodes[i-1])
+        G.add_edge(list_nodes[i],list_nodes[i-2])
+        target=i+1
+        if target>n-1:
+            target=target-n
+            G.add_edge(list_nodes[i],target)
+        else:
+            G.add_edge(list_nodes[i],target)
+        target=i+2
+        if target>n-1:
+            target=target-n
+            G.add_edge(list_nodes[i],target)
+        else:
+            G.add_edge(list_nodes[i],target)
+            
 G=nx.Graph()
 G.add_nodes_from(range(0,50))
-nx.draw(G)
+
+add_edges(G)  #add ties based on homophily
+for each in G.nodes():
+    print each,':',
+    for each1 in G.neighbors(each):
+        print each1,
+    print '\n'
+nx.draw(G,with_labels=1)
 plt.show()
